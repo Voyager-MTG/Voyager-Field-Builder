472ea3db2580b3ee68531ba37ff3af5090a4b407
uiwow7 <aydinanginer087@gmail.com>
2025-06-22 17:49:33 -0700

    big uppdate: tokenpuller

--- trice-merge/merge.py
+++ trice-merge/merge.py
@@ -1,23 +1,39 @@
-import os, shutil, xml.etree.ElementTree as XET, regex
+import os, shutil, xml.etree.ElementTree as XET, re
 
 TOKENMATCHES = {
     "Can Be Cast From Exile EXPT": "(play|cast) ([^\n.]+ (from exile|exiled)|(one of )?those cards|them|it this turn|it until)",
     "Embraced Cards AKT": "(E|e)mbrace",
-    "Trace Reminder VNM": "(C|c)reate (a|two|three|four|five|X) trace",
+    "Liberated Dragon AKT": "(L|l)iberate",
+    "Trace Reminder VNM": "(C|c)reate (a|two|three|four|five|X) trace(s?) of",
     "Vertex Reminder PVR": "{V",
     "Luminous VNM": "(L|l)uminous",
-    "Reserves HOD": "(I|i)n your reserves"
+    "Reserves HOD": "(I|i)n your reserves",
+    "~ Token WAW": "Reflect {[0-9WUBRGIXYZ]+?}",
+    "~ ITD": "Iterate"
 }
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            # print("copying", s, d)
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 def liststr(l):
     s = ""
     for i in l:
-        s += i
+        if i != None:
+            s += i
     return s
 
+print("Initializing...")
 
 try:
     shutil.rmtree("export")
+    shutil.rmtree("temp")
 except:
     pass
 
@@ -27,9 +43,11 @@ try:
     os.mkdir("export")
     os.mkdir("export/pics")
     os.mkdir("export/pics/tokens")
+    os.mkdir("temp")
 except:
     print("Directory already exists")
 
+copytree("sets", "temp")
 # time.sleep(3)
 
 sets_text = ""
@@ -39,7 +57,7 @@ out_text_cards = ""
 out_text_tokens = ""
 uuid = 0
 
-for set_file_path in os.listdir('sets'): # Loop through the sets folder
+for set_file_path in os.listdir('temp'): # Loop through the temp folder
     # Initialize variables
     set_name = ""
     set_folder = False
@@ -52,7 +70,7 @@ for set_file_path in os.listdir('sets'): # Loop through the sets folder
     set_xml = not set_folder # set a bool telling us if we're in a set xml file
 
     if set_xml: # If we're in an xml
-        with open(f"sets/{set_file_path}", "r", encoding="utf-8") as f: # open up the xml and read the text
+        with open(f"temp/{set_file_path}", "r", encoding="utf-8") as f: # open up the xml and read the text
             xml_text = f.read()
         
         set_card_text = "<cards>" + (xml_text.split("<cards>"))[1].split("</cards>")[0] + "</cards>"
@@ -62,7 +80,6 @@ for set_file_path in os.listdir('sets'): # Loop through the sets folder
             name = card[0].text
             set = card[1].text
             rar = card[1].attrib["rarity"]
-            # print(f'<name>{name}</name><set rarity="{rar}">{set}</set>')
             set_card_text = set_card_text.replace(f'<name>{name}</name>\n      <set rarity="{rar}">{set}</set>', f'<name>{name}</name>\n      <set rarity="{rar}" uuid="{uuid}">{set}</set>')
             uuid += 1
 
@@ -71,12 +88,12 @@ for set_file_path in os.listdir('sets'): # Loop through the sets folder
         sets_text += (xml_text.split("<sets>"))[1].split("</sets>")[0]  # add the sets to sets_text
         cards_text += set_card_text  # add the cards to cards_text
     else: # If we're in a folder
-        with open(f"sets/{set_file_path}/{set_name} Tokens.xml", "r", encoding="utf-8") as f:
+        with open(f"temp/{set_file_path}/{set_name} Tokens.xml", "r", encoding="utf-8") as f:
             xml_text = f.read()  # open up the tokens xml and read the text
         
         tokens_text += (xml_text.split("<cards>"))[1].split("</cards>")[0] # get the card text and put it into tokens_text
         
-        os.rename(f"sets/{set_file_path}", f"export/pics/{set_name}")
+        os.rename(f"temp/{set_file_path}", f"export/pics/{set_name}")
 
     print(f"Parsed {set_file_path}")
 
@@ -102,14 +119,12 @@ out_text_cards += """</cards>
 """
 
 cards_xml.write(out_text_cards)
-# cards_xml.close()
 
 out_text_tokens += """<?xml version="1.0" encoding="UTF-8"?>
 <cockatrice_carddatabase version="4">
   <cards>
 """
 
-# print(tokens_text)
 out_text_tokens += tokens_text
 
 out_text_tokens += """</cards>
@@ -117,31 +132,123 @@ out_text_tokens += """</cards>
 """
 
 cards_root = XET.parse("export/cards.xml").getroot()
+tokens_root = XET.fromstring(out_text_tokens)
 
-# print()
+def englishToNumber(s):
+    n = 1
 
-print("Parsing reminders")
-for card in cards_root[1]:
-    rules_text = ""
-    card_name = ""
-    for property in card:
-        if property.tag == "text":
-            rules_text = property.text
-        elif property.tag == "name":
-            card_name = property.text
+    if "one" == s or "a" == s or "an" == s:
+        n = 1
+    elif "two" == s:
+        n = 2
+    elif "three" == s:
+        n = 3
+    elif "four" == s:
+        n = 4
+    elif "five" == s:
+        n = 5
+    elif "six" == s:
+        n = 6
+    elif "seven" == s:
+        n = 7
+    elif "eight" == s:
+        n = 8
+    elif "nine" == s:
+        n = 9
+    elif "ten" == s:
+        n = 10
+    elif "eleven" == s:
+        n = 11
+    elif "twelve" == s:
+        n = 12
+    elif "thirteen" == s:
+        n = 13
+    elif "fourteen" == s:
+        n = 14
+    elif "fifteen" == s:
+        n = 15
+    elif "sixteen" == s:
+        n = 16
+    elif "seventeen" == s:
+        n = 17
+    elif "eighteen" == s:
+        n = 18
+    elif "nineteen" == s:
+        n = 19
+    elif "twenty" == s:
+        n = 20
+    elif "x" in s or "that many" in s or "a number" in s:
+        n = "x"
+
+    return n
+
+def getCount(n):
+    if n != 1:
+        return f"count=\"{n}\""
+    else:
+        return ""
+
+def tokenPuller(card):
+    global out_text_tokens
+    big_match = re.findall(r'creates? [^.]+', card["text"], flags=re.I)
+    if big_match and not "!noscript" in card["notes"]:
+        for phrase in big_match:
+            token_regex = re.compile("[C|c]reate (X|X plus one|a number of|that many|a|an|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen|twenty)( tapped| goaded)?( and attacking)?( legendary)?( basic)?( snow)? ?([XYZ0-9]+/[XYZ0-9]+ )?(colorless|white|blue|black|red|green|silver)?(, (?:white|blue|black|red|green|silver),)?( and white| and blue| and black| and red| and green| and silver)? ?([A-Z][a-z]+)?( [A-Z][a-z]+)?( [A-Z][a-z]+)? ?(enchantment )?(artifact )?(land )?(creature )?tokens?( (with|named|that[’']s|that is|that are|attached|that can't block) [^\n.]+)?")
+            tokens = token_regex.search(phrase)
+            if tokens:
+                tokenMatch = tokens.groups()
+                token_type = liststr(tokenMatch[10:13])
+                count = englishToNumber(tokenMatch[0])
+                if token_type == "":
+                    token_name = re.findall("[N|n]amed (.*?) (with|that)", str(tokenMatch[17]))
+                    if token_name:
+                        token_to_script = token_name[0][0] + " " + card["set"]
+                        out_text_tokens = out_text_tokens.replace(f"<name>{token_to_script}</name>", f"<name>{token_to_script}</name>\n      <reverse-related {getCount(count)}>{card["name"]}</reverse-related>")
+                else: 
+                    token_to_script = token_type + " " + card["set"]
+                    out_text_tokens = out_text_tokens.replace(f"<name>{token_to_script}</name>", f"<name>{token_to_script}</name>\n      <reverse-related {getCount(count)}>{card["name"]}</reverse-related>")
+                    token_to_script = token_type + " Token " + card["set"]
+                    out_text_tokens = out_text_tokens.replace(f"<name>{token_to_script}</name>", f"<name>{token_to_script}</name>\n      <reverse-related {getCount(count)}>{card["name"]}</reverse-related>")
+    
+    # Manual Scripts
+    
+                    
+
+print("Building tokens...")
+for card_ in cards_root[1]:
+    card = {}
+    for property in card_:
+        if property == "prop":
+            for prop in property:
+                card[prop.tag] = prop.text
+        else:
+            card[property.tag] = property.text
+
+    card["notes"] = ""
+
+    if card["text"]: 
+        tokenPuller(card)
 
     for token, exp in TOKENMATCHES.items():
-        # print(exp, rules_text)
-        if not rules_text or rules_text == None or rules_text == "":
+        if not card["text"] or card["text"] == None or card["text"] == "":
             continue
-        # print("looking", regex.findall(exp, rules_text))
-        if regex.findall(exp, rules_text):
-            out_text_tokens = out_text_tokens.replace(f"<name>{token}</name>", f"<name>{token}</name>\n      <reverse-related>{card_name}</reverse-related>")
-            # print(out_text_tokens)
+
+        token = token.replace("~", card["name"])
+        token = token.replace("CARDNAME", card["name"])
+        # token = token.replace("CARDTYPE", card["type"])
+        token = token.replace("CARDSET" , card["set" ])
+        
+        if re.findall(exp, card["text"]):
+            out_text_tokens = out_text_tokens.replace(f"<name>{token}</name>", f"<name>{token}</name>\n      <reverse-related>{card["name"]}</reverse-related>")
 
 tokens_xml.write(out_text_tokens)
 
 tokens_xml.close()
 cards_xml.close()
+
+try:
+    shutil.rmtree("temp")
+except:
+    pass
 
 print("Done!")
